04426cefee833d4d992fefee751858bfaf6cb3ab
Guilherme <meloguilherme1994@gmail.com>
2023-01-30 10:50:44 -0300

    Add crossoring

--- angular-spring-ecommerce/backend/ecommerce-web/src/main/java/io/github/guilhermemelo01/ecommerceweb/dao/ProductRepository.java
+++ angular-spring-ecommerce/backend/ecommerce-web/src/main/java/io/github/guilhermemelo01/ecommerceweb/dao/ProductRepository.java
@@ -2,6 +2,8 @@ package io.github.guilhermemelo01.ecommerceweb.dao;
 
 import io.github.guilhermemelo01.ecommerceweb.entinty.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
