779d86cbf06121c82c99321ef270b0fb2cc94d20
Guilherme <meloguilherme1994@gmail.com>
2023-01-31 10:07:58 -0300

    Styling the homepage with bootstrap

--- angular-spring-ecommerce/backend/ecommerce-web/src/main/java/io/github/guilhermemelo01/ecommerceweb/EcommerceWebApplication.java
+++ angular-spring-ecommerce/backend/ecommerce-web/src/main/java/io/github/guilhermemelo01/ecommerceweb/EcommerceWebApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EcommerceWebApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(EcommerceWebApplication.class, args);
     }
