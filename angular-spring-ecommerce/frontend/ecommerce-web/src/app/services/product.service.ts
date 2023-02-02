a5aabd46daafe9149bfd35dce4790abdd2855cea
Guilherme <meloguilherme1994@gmail.com>
2023-02-02 14:50:49 -0300

    Dynamic search component

--- angular-spring-ecommerce/frontend/ecommerce-web/src/app/services/product.service.ts
+++ angular-spring-ecommerce/frontend/ecommerce-web/src/app/services/product.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map, Observable } from 'rxjs';
 import { Product } from '../common/product';
+import { ProductCategory } from '../common/product-category';
 
 @Injectable({
   providedIn: 'root'
@@ -9,11 +10,11 @@ import { Product } from '../common/product';
 export class ProductService {
 
   private baseUrl = 'http://localhost:8080/api/products';
+  private CategoryUrl = 'http://localhost:8080/api/product-category';
 
   constructor(private httpClient: HttpClient) { }
 
   getProductList(theCategoryId: number): Observable<Product[]> {
-
     // need to build URL based on category id
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
 
@@ -21,10 +22,22 @@ export class ProductService {
       map(response => response._embedded.products)
     );
   }
+
+  getProductCategories(): Observable<ProductCategory[]>{
+    return this.httpClient.get<GetReponseProductCategory>(this.CategoryUrl).pipe(
+      map(response => response._embedded.productCategory)
+    );
+  }
 }
 
 interface GetResponse {
   _embedded: {
     products: Product[];
+  }
+}
+
+interface GetReponseProductCategory{
+  _embedded:{
+    productCategory: ProductCategory[];
   }
 }
