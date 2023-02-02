d99db3459b9d868de9d14127e8a8588b43407078
Guilherme <meloguilherme1994@gmail.com>
2023-02-02 13:59:14 -0300

    Expose entity ids

--- angular-spring-ecommerce/backend/ecommerce-web/src/main/java/io/github/guilhermemelo01/ecommerceweb/config/MyDataRestConfiguration.java
+++ angular-spring-ecommerce/backend/ecommerce-web/src/main/java/io/github/guilhermemelo01/ecommerceweb/config/MyDataRestConfiguration.java
@@ -2,15 +2,28 @@ package io.github.guilhermemelo01.ecommerceweb.config;
 
 import io.github.guilhermemelo01.ecommerceweb.entinty.Product;
 import io.github.guilhermemelo01.ecommerceweb.entinty.ProductCategory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.EntityType;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 @Configuration
 public class MyDataRestConfiguration implements RepositoryRestConfigurer {
+    
+    private EntityManager entityManager;
 
+    @Autowired
+    public MyDataRestConfiguration(EntityManager entityManager){
+        this.entityManager = entityManager;
+    }
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors){
         HttpMethod [] theUnsupportedActions = {HttpMethod.POST, HttpMethod.PUT, HttpMethod.DELETE};
@@ -26,6 +39,29 @@ public class MyDataRestConfiguration implements RepositoryRestConfigurer {
                 .forDomainType(ProductCategory.class)
                 .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)))
                 .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)));
+
+        exposeIds(config);
     }
+
+    private void exposeIds(RepositoryRestConfiguration config){
+
+        //Expose entity ids
+        
+        // -gets a list of all entity classes from the entity manager
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+
+        // - create an array of the entity types
+        List<Class> entityClasses = new ArrayList<>();
+
+        // - get the entity types for entities
+        for(EntityType tempEntity: entities){
+            entityClasses.add(tempEntity.getJavaType());
+        }
+
+        // - expose the entity ids for the array of entity/domain types
+        Class[] domainTypes = entityClasses.toArray(new Class[0]);
+        config.exposeIdsFor(domainTypes);
+    }
+
 
 }
--- angular-spring-ecommerce/backend/ecommerce-web/src/main/java/io/github/guilhermemelo01/ecommerceweb/dao/ProductRepository.java
+++ angular-spring-ecommerce/backend/ecommerce-web/src/main/java/io/github/guilhermemelo01/ecommerceweb/dao/ProductRepository.java
@@ -1,9 +1,14 @@
 package io.github.guilhermemelo01.ecommerceweb.dao;
 
 import io.github.guilhermemelo01.ecommerceweb.entinty.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    Page<Product> findByCategoryId(@Param("id")  Long id, Pageable pageable);
 }
