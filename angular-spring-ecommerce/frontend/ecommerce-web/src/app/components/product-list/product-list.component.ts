c3ff9b2b1a0355ffb4d88ef5577cd5f07572dae3
Guilherme <meloguilherme1994@gmail.com>
2023-02-09 14:56:54 -0300

    created Selecting page size

--- angular-spring-ecommerce/frontend/ecommerce-web/src/app/components/product-list/product-list.component.ts
+++ angular-spring-ecommerce/frontend/ecommerce-web/src/app/components/product-list/product-list.component.ts
@@ -76,6 +76,12 @@ export class ProductListComponent implements OnInit {
       });
   }
 
+  updatePageSize(pageSize: string){
+    this.thePageSize = +pageSize;
+    this.thePageNumber = 1;
+    this.listProducts();
+  }
+
   handleSearchProducts() {
     //Search
     const theKeyword: string = this.route.snapshot.paramMap.get('keyword')!;
