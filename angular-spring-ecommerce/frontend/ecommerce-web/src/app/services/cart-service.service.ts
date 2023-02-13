75bd785f2cdcf8821dd566ae6874672400116a6a
Guilherme <meloguilherme1994@gmail.com>
2023-02-13 13:46:54 -0300

    Created new method for my cartService

--- angular-spring-ecommerce/frontend/ecommerce-web/src/app/services/cart-service.service.ts
+++ angular-spring-ecommerce/frontend/ecommerce-web/src/app/services/cart-service.service.ts
@@ -7,11 +7,42 @@ import { CartItem } from '../common/cart-item';
 })
 export class CartServiceService {
 
-  cartItens: CartItem[] = []  
+  cartItens: CartItem[] = []
 
   totalPrice: Subject<number> = new Subject<number>();
   totalQuantity: Subject<number> = new Subject<number>();
 
-
   constructor() { }
+
+
+  addToCart(theCartItens: CartItem) {
+
+    //Check if we already have the item in our cart
+    let alreadyExistsInCart: boolean = false;
+    let existingCartItens: CartItem = undefined!;
+
+    if (this.cartItens.length > 0) {
+      //find the item in the cart based on item id
+      for (let tempCartItem of this.cartItens) {
+        if (tempCartItem.id === theCartItens.id) {
+          existingCartItens = tempCartItem;
+          break;
+        }
+      }
+    }
+
+    //check if we found it
+    alreadyExistsInCart = (existingCartItens != undefined);
+
+    if (alreadyExistsInCart) {
+      //increment the quantity
+      existingCartItens.quantity++;
+    } else {
+      this.cartItens.push(theCartItens);
+    }
+
+    //compute cart quantity and cart total
+    // this.computeCartTotals();
+  }
+
 }
