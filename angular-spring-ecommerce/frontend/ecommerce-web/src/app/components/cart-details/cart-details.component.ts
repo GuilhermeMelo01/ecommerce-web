1b7e287d53a9a3b1647bb04a22a7ace24b7eb32b
Guilherme <meloguilherme1994@gmail.com>
2023-02-17 13:48:44 -0300

    Added new button to Increment

--- angular-spring-ecommerce/frontend/ecommerce-web/src/app/components/cart-details/cart-details.component.ts
+++ angular-spring-ecommerce/frontend/ecommerce-web/src/app/components/cart-details/cart-details.component.ts
@@ -37,4 +37,7 @@ export class CartDetailsComponent implements OnInit{
       this.cartService.computeCartTotals();
   }
 
+  incrementQuantity(theCartItem: CartItem){
+      this.cartService.addToCart(theCartItem);
+  }
 }
