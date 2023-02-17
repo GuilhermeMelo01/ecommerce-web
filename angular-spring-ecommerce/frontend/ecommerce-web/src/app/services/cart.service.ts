1a4aea642a9a72eecaed7c7da247b14ede4243c6
Guilherme <meloguilherme1994@gmail.com>
2023-02-17 13:59:27 -0300

    Decrement item method

--- angular-spring-ecommerce/frontend/ecommerce-web/src/app/services/cart.service.ts
+++ angular-spring-ecommerce/frontend/ecommerce-web/src/app/services/cart.service.ts
@@ -14,7 +14,6 @@ export class CartService {
 
   constructor() { }
 
-
   addToCart(theCartItens: CartItem) {
 
     //Check if we already have the item in our cart
@@ -52,6 +51,30 @@ export class CartService {
     //publish the new values... all subscribes will receive the new data
     this.totalPrice.next(totalValueItens);
     this.totalQuantity.next(totalQuantityItens);
+  }
+
+  decrementQuantity(theCartItem: CartItem){
+      theCartItem.quantity--;
+
+      if(theCartItem.quantity == 0){
+        this.remove(theCartItem);
+      }else{
+        this.computeCartTotals();
+      }
+  }
+  
+  remove(theCartItem: CartItem){
+    // get index of item in the array
+    const itemIndex = this.cartItems.findIndex(
+      tempCartItem => tempCartItem.id === theCartItem.id
+    );
+
+    // if found, remove the item from the array at the given index
+    if(itemIndex > -1){
+      this.cartItems.splice(itemIndex, 1);
+
+      this.computeCartTotals();
+    }
   }
 
 }
