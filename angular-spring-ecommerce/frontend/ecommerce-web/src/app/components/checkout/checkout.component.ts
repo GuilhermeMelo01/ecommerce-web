48fd0243e805b70d99e210990cd85568829e1041
Guilherme <meloguilherme1994@gmail.com>
2023-02-23 10:58:24 -0300

    Added event to form submission

--- angular-spring-ecommerce/frontend/ecommerce-web/src/app/components/checkout/checkout.component.ts
+++ angular-spring-ecommerce/frontend/ecommerce-web/src/app/components/checkout/checkout.component.ts
@@ -25,7 +25,6 @@ export class CheckoutComponent implements OnInit {
   onSubmit(){
     console.log("Handling the submit button");
     console.log(this.checkoutFormGroup.get('customer')?.value);
-    
   }
 
 }
