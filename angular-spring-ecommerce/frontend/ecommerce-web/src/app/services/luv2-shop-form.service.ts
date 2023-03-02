37df5e04e33f1b2a84ca14a6c374448a8c2946b7
Guilherme <meloguilherme1994@gmail.com>
2023-03-02 10:10:08 -0300

    Created methods in service angular to getCountries and getStates

--- angular-spring-ecommerce/frontend/ecommerce-web/src/app/services/luv2-shop-form.service.ts
+++ angular-spring-ecommerce/frontend/ecommerce-web/src/app/services/luv2-shop-form.service.ts
@@ -1,6 +1,8 @@
+import { Country } from './../common/country';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable, of, map } from 'rxjs';
+import { State } from '../common/state';
 
 @Injectable({
   providedIn: 'root'
@@ -12,31 +14,53 @@ export class Luv2ShopFormService {
 
   constructor(private httpClient: HttpClient) { }
 
-  getCreditCardMonths(startMonth: number): Observable<number[]>{
-     let data: number[] = [];
-     //build an array for "Month" dropdown list
-     // - Start at current month and  loop until month number 12
-     for(let tempMonth = startMonth; tempMonth <= 12; tempMonth++){
-        data.push(tempMonth);
-     }
+  getCountries(): Observable<Country[]> {
 
-     return of(data);
+    return this.httpClient.get<GetResponseCountries>(this.countriesUrl).pipe(
+      map(response => response._embedded.countries));
+  }
+
+  getStates(theCodeCountry: string): Observable<State[]> {
+    const searchUrl = `${this.statesUrl}/search/findByCountryCode?code=${theCodeCountry}`
+    return this.httpClient.get<GetResponseStates>(searchUrl).pipe(
+      map(response => response._embedded.states));
+  }
+
+  getCreditCardMonths(startMonth: number): Observable<number[]> {
+    let data: number[] = [];
+    //build an array for "Month" dropdown list
+    // - Start at current month and  loop until month number 12
+    for (let tempMonth = startMonth; tempMonth <= 12; tempMonth++) {
+      data.push(tempMonth);
+    }
+
+    return of(data);
   }
 
   getCreditCardYear(): Observable<number[]> {
     let data: number[] = [];
 
     const startYear: number = new Date().getFullYear();
-    const endYear: number = startYear+10;
+    const endYear: number = startYear + 10;
 
-    for(let tempYear = startYear; tempYear <= endYear; tempYear++){
+    for (let tempYear = startYear; tempYear <= endYear; tempYear++) {
       data.push(tempYear);
     }
 
     return of(data);
   }
+}
 
+interface GetResponseCountries {
+  _embedded: {
+    countries: Country[];
+  }
+}
 
+interface GetResponseStates {
+  _embedded: {
+    states: State[];
+  }
 }
 
 
