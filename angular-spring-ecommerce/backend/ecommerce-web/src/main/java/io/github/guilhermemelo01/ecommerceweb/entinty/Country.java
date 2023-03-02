2385e8e5bc6ae7af9aa1f746357e44b6b03023ed
Guilherme <meloguilherme1994@gmail.com>
2023-03-02 09:08:37 -0300

    Retrieved the countries data

--- angular-spring-ecommerce/backend/ecommerce-web/src/main/java/io/github/guilhermemelo01/ecommerceweb/entinty/Country.java
+++ angular-spring-ecommerce/backend/ecommerce-web/src/main/java/io/github/guilhermemelo01/ecommerceweb/entinty/Country.java
@@ -1,5 +1,6 @@
 package io.github.guilhermemelo01.ecommerceweb.entinty;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -24,6 +25,7 @@ public class Country {
     private String name;
 
     @OneToMany(mappedBy = "country")
+    @JsonIgnore
     private List<State> states;
 
 
