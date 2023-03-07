e9e207c92aaeafa376f3eebc99bc76c1661a5048
Guilherme <meloguilherme1994@gmail.com>
2023-03-07 10:28:59 -0300

    Populate Countries and States

--- angular-spring-ecommerce/frontend/ecommerce-web/src/app/components/checkout/checkout.component.ts
+++ angular-spring-ecommerce/frontend/ecommerce-web/src/app/components/checkout/checkout.component.ts
@@ -90,13 +90,25 @@ export class CheckoutComponent implements OnInit {
   onSubmit() {
     console.log("Handling the submit button");
     console.log(this.checkoutFormGroup.get('customer')?.value);
+
+    console.log("The email address is "+ this.checkoutFormGroup.get('customer')?.value.email)
+
+    console.log("The shipping address country is "+ this.checkoutFormGroup.get('shippingAddress')?.value.country.name);
+
+    console.log("The shipping address state is "+ this.checkoutFormGroup.get('shippingAddress')?.value.state.name);
   }
 
   copyShippingAddressToBillingAddress(event: any) {
     if (event.target.checked) {
       this.checkoutFormGroup.controls.billingAddress.setValue(this.checkoutFormGroup.controls.shippingAddress.value);
+
+      // bug fix for states
+      this.billingAddressStates = this.shippingAddressStates;
     } else {
       this.checkoutFormGroup.controls.billingAddress.reset();
+
+      // bug fix for states
+      this.billingAddressStates = [];
     }
   }
 
@@ -130,8 +142,8 @@ export class CheckoutComponent implements OnInit {
     const countryCode = formGroup?.value.country.code;
     const countryName = formGroup?.value.country.name;
 
-    console.log(`{formGroupName} Country code: ${countryCode}`);
-    console.log(`{formGroupName} Country name: ${countryName}`);
+    console.log(`${formGroupName} Country code: ${countryCode}`);
+    console.log(`${formGroupName} Country name: ${countryName}`);
 
     this.luv2ShopFormService.getStates(countryCode).subscribe(
       data => {
