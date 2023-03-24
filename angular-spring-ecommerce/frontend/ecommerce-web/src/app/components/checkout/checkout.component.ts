46ae9b152468eb22fed5f7c56b0d93b09606eedf
Guilherme <meloguilherme1994@gmail.com>
2023-03-24 08:04:34 -0300

    frontend - Called the API service

--- angular-spring-ecommerce/frontend/ecommerce-web/src/app/components/checkout/checkout.component.ts
+++ angular-spring-ecommerce/frontend/ecommerce-web/src/app/components/checkout/checkout.component.ts
@@ -190,8 +190,35 @@ export class CheckoutComponent implements OnInit {
     purchase.billingAddress.country = billingCountry.name;
 
     //populate purchase - order and OrderItems
+    purchase.order = order;
+    purchase.orderItems = orderItems;
 
     // call REST API via the CheckoutService
+    this.checkoutService.placeOrder(purchase).subscribe({
+        next: response => {
+          alert(`Your order has been received.\nOrder Trancking number: ${response.orderTrackingNumber}`)
+
+          //resert cart
+          this.resetCart();
+        },
+        error: err => {
+          alert(`There was an error: ${err.message}`);
+        }
+      }
+    );
+  }
+
+  resetCart() {
+    //reset cart data
+    this.cartService.cartItems = [];
+    this.cartService.totalPrice.next(0);
+    this.cartService.totalQuantity.next(0);
+
+    //reset the form
+    this.checkoutFormGroup.reset();
+
+    //navigate back to the products page
+    this.router.navigateByUrl("/products");
   }
 
   copyShippingAddressToBillingAddress(event: any) {
