56c62c51964baca43ba4f3ab180776dc2a37490e
Guilherme <meloguilherme1994@gmail.com>
2023-03-29 10:20:00 -0300

    Created login component

---
+++ angular-spring-ecommerce/frontend/ecommerce-web/src/app/components/login/login.component.ts
@@ -0,0 +1,36 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { inject } from '@angular/core/testing';
+import { OKTA_AUTH } from '@okta/okta-angular';
+import { OktaAuth } from '@okta/okta-auth-js';
+import OktaSignIn from '@okta/okta-signin-widget';
+
+import myAppConfig from 'src/app/config/my-app-config';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  oktaSignin: any;
+
+  constructor(@Inject(OKTA_AUTH) private oktaAuth: OktaAuth) {
+
+    this.oktaSignin = new OktaSignIn({
+      logo: 'assets/images/products/logo.png', //Possible erro in logo path
+      baseUrl: myAppConfig.oidc.issuer.split('/oauth2')[0],
+      clientId: myAppConfig.oidc.clientId,
+      redirectUri: myAppConfig.oidc.redirectUri,
+      authParams: {
+        pkce: true,
+        issuer: myAppConfig.oidc.issuer,
+        scopes: myAppConfig.oidc.scopes
+      }
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+}
